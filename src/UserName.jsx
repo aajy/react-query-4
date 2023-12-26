34015005a7259aa54bf92782b838bb5cfc3427e3
aajy <aajy.000@gmail.com>
2023-12-26 13:04:50 +0900

    리액트 쿼리로 데이터 호출, useUserQuery.js에 react-query-devtool 상태값 reefetchOnWindowFocus, refetchOnMount, cacheTime

--- src/UserName.jsx
+++ src/UserName.jsx
@@ -1,4 +1,8 @@
+import { useUserQuery } from './hooks/useUserQuery';
+
 export default function UserName() {
+	const result = useUserQuery();
+	console.log(result);
 	return (
 		<div className='UserName'>
 			<h1>User Name</h1>
